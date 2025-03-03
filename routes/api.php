7a1c5bd7bbfdfa33971d4cb57e83ff340b22e333
twoclicks-clickmaps <github@clickmaps.com.br>
2025-03-02 21:27:37 -0300

    Atualização routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -25,28 +25,28 @@ Route::middleware([VerifyHeaders::class])->group(function () {
     Route::get('/credential/{credential}', [CredentialController::class, 'show']); // Mostra uma credencial específica
     Route::post('/credential', [CredentialController::class, 'store']); // Cria uma nova credencial
     Route::put('/credential/{credential}', [CredentialController::class, 'update']); // Atualiza uma credencial existente
-    Route::delete('/credential/{credential}', [CredentialController::class, 'distroy']); // Deleta uma credencial
+    Route::delete('/credential/{credential}', [CredentialController::class, 'destroy']); // Deleta uma credencial
 
     // Rotas para Person
     Route::get('/person', [PersonController::class, 'index']); // Lista todas as pessoas
     Route::get('/person/{person}', [PersonController::class, 'show']); // Mostra uma pessoa específica
     Route::post('/person', [PersonController::class, 'store']); // Cria uma nova pessoa
     Route::put('/person/{person}', [PersonController::class, 'update']); // Atualiza uma pessoa existente
-    Route::delete('/person/{person}', [PersonController::class, 'distroy']); // Deleta uma pessoa
+    Route::delete('/person/{person}', [PersonController::class, 'destroy']); // Deleta uma pessoa
 
     // Rotas para PersonAddress
     Route::get('/person_address', [PersonAddressController::class, 'index']); // Lista todos os endereços
     Route::get('/person_address/{person_address}', [PersonAddressController::class, 'show']); // Mostra um endereço específico
     Route::post('/person_address', [PersonAddressController::class, 'store']); // Cria um novo endereço
     Route::put('/person_address/{person_address}', [PersonAddressController::class, 'update']); // Atualiza um endereço existente
-    Route::delete('/person_address/{person_address}', [PersonAddressController::class, 'distroy']); // Deleta um endereço
+    Route::delete('/person_address/{person_address}', [PersonAddressController::class, 'destroy']); // Deleta um endereço
 
     // Rotas para PersonUser
     Route::get('/person_user', [PersonUserController::class, 'index']); // Lista todos os usuários
     Route::get('/person_user/{person_user}', [PersonUserController::class, 'show']); // Mostra um usuário específico
     Route::post('/person_user', [PersonUserController::class, 'store']); // Cria um novo usuário
     Route::put('/person_user/{person_user}', [PersonUserController::class, 'update']); // Atualiza um usuário existente
-    Route::delete('/person_user/{person_user}', [PersonUserController::class, 'distroy']); // Deleta um usuário
+    Route::delete('/person_user/{person_user}', [PersonUserController::class, 'destroy']); // Deleta um usuário
 
     Route::get('/check_email/{id}', [PersonUserController::class, 'check_email']);
 
@@ -55,28 +55,28 @@ Route::middleware([VerifyHeaders::class])->group(function () {
     Route::get('/gender/{gender}', [GenderController::class, 'show']); // Mostra um gênero específico
     Route::post('/gender', [GenderController::class, 'store']); // Cria um novo gênero
     Route::put('/gender/{gender}', [GenderController::class, 'update']); // Atualiza um gênero existente
-    Route::delete('/gender/{gender}', [GenderController::class, 'distroy']); // Deleta um gênero
+    Route::delete('/gender/{gender}', [GenderController::class, 'destroy']); // Deleta um gênero
 
     // Rotas para Video
     Route::get('/video', [VideoController::class, 'index']); // Lista todos os videos
     Route::get('/video/{video}', [VideoController::class, 'show']); // Mostra um video específico
     Route::post('/video', [VideoController::class, 'store']); // Cria um novo video
     Route::put('/video/{video}', [VideoController::class, 'update']); // Atualiza um video existente
-    Route::delete('/video/{video}', [VideoController::class, 'distroy']); // Deleta um video
+    Route::delete('/video/{video}', [VideoController::class, 'destroy']); // Deleta um video
 
     // Rotas para Partilha
     Route::get('/share', [ShareController::class, 'index']); // Lista todos os Partilhas
     Route::get('/share/{share}', [ShareController::class, 'show']); // Mostra uma Partilha específico
     Route::post('/share', [ShareController::class, 'store']); // Cria um novo Partilha
     Route::put('/share/{share}', [ShareController::class, 'update']); // Atualiza uma Partilha existente
-    Route::delete('/share/{share}', [ShareController::class, 'distroy']); // Deleta uma Partilha
+    Route::delete('/share/{share}', [ShareController::class, 'destroy']); // Deleta uma Partilha
 
     // Rotas para TypePerson
     Route::get('/type_person', [TypePersonController::class, 'index']); // Lista todos os tipos de pessoa
     Route::get('/type_person/{type_person}', [TypePersonController::class, 'show']); // Mostra um tipo de pessoa específico
     Route::post('/type_person', [TypePersonController::class, 'store']); // Cria um novo tipo de pessoa
     Route::put('/type_person/{type_person}', [TypePersonController::class, 'update']); // Atualiza um tipo de pessoa existente
-    Route::delete('/type_person/{type_person}', [TypePersonController::class, 'distroy']); // Deleta um tipo de pessoa
+    Route::delete('/type_person/{type_person}', [TypePersonController::class, 'destroy']); // Deleta um tipo de pessoa
 
 });
 
